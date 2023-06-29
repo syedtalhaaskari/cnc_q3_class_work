4cd54f63a389269aa2013bf4f95c68877de33007
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-06-29 15:24:12 +0500

    Every Page can has its own layout

---
+++ src/app/about/layout.js
@@ -0,0 +1,10 @@
+const AboutLayout = ({children}) => {
+  return (
+    <div>
+        {children}
+        <div className="container">This is about footer</div>
+    </div>
+  )
+}
+
+export default AboutLayout
