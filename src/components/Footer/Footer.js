a07bf02ebbea163fdbd39ebda0d7744630bd1bf7
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-06-29 15:33:35 +0500

    Create Footer and style it

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+"use client"
+
+import Link from "next/link"
+import styles from "./Footer.module.css";
+import { usePathname } from "next/navigation";
+
+const Footer = () => {
+    const pathname = usePathname()
+
+    return (
+        pathname !== "/about" ? <nav className={styles.navContainer}>
+            Footer
+            {/* <ul className={styles.navbar + " container"}>
+                <li><Link className={`${styles.navLink} ${pathname === "/" && styles.active}`} href={"/"}>Home</Link></li>
+                <li><Link className={`${styles.navLink} ${pathname === "/portfolio" && styles.active}`} href={"/portfolio"}>Portfolio</Link></li>
+                <li><Link className={`${styles.navLink} ${pathname === "/portfolio/nhaps" && styles.active }`} href={"/portfolio/nhaps"}>Nhaps</Link></li>
+                <li><Link className={`${styles.navLink} ${pathname === "/portfolio/hkk" && styles.active }`} href={"/portfolio/hkk"}>Hkk</Link></li>
+            </ul> */}
+        </nav> : <></>
+    )
+}
+
+export default Footer
